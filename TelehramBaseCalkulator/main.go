4bbca40ea5fbecdc7183c00c40efbece9a0c0f87
Mirik <mirrchik98@gmail.com>
2020-02-07 04:07:57 +0200

    Added buttons, empty for now

--- TelehramBaseCalkulator/main.go
+++ TelehramBaseCalkulator/main.go
@@ -18,47 +18,58 @@ func main() {
 	u.Timeout = 60
 	u.Limit = 10
 	u.Offset = 0
-	updChan, err := bot.GetUpdatesChan(u)
+	updates, err := bot.GetUpdatesChan(u)
 	if err != nil {
 		log.Panic("update error:", err)
 	}
+	var Menu = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Base Convertion"),
+			tgbotapi.NewKeyboardButton("Base Calkulator"),
+			tgbotapi.NewKeyboardButton("Usuall Calkulator"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Help"),
+			tgbotapi.NewKeyboardButton("Web Version"),
+			tgbotapi.NewKeyboardButton("Button"),
+		),
+	)
+	for update := range updates {
 
-	for {
-		update := <-updChan
-		if update.Message.IsCommand() == true && update.Message.Command() != "Calculate" {
-			cmd := update.Message.Command()
-			if cmd == "help" {
-				msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, "there is nothing yet")
-				bot.Send(msgAnswer)
-			}
-			if cmd == "start" {
-				msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello to you")
-				bot.Send(msgAnswer)
-			}
+		cmd := update.Message.Command()
+
+		if cmd == "start" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "There menu:")
+			msg.ReplyMarkup = Menu
+			bot.Send(msg)
 		}
-		//Todo: there i am trying to get separeted user request's
-		if update.Message.Command() == "Calculate" {
-			msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, "Okay, lets start calculating, please write first number")
-			bot.Send(msgAnswer)
-			for {
-				if update.Message.Text == "" {
-					msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-					bot.Send(msgAnswer)
 
-				}
-				if update.Message.Text == "Hey" {
-					msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, "Hey to you")
-					bot.Send(msgAnswer)
-					break
-				}
-			}
-
-		} else {
-			if update.Message.Text != "" {
-				msgAnswer := tgbotapi.NewMessage(update.Message.Chat.ID, "hmmm")
-				bot.Send(msgAnswer)
-			}
+		if update.Message.Text == Menu.Keyboard[0][0].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Okay, lets start calculating, please write first number")
+			bot.Send(msg)
 		}
+
+		if update.Message.Text == Menu.Keyboard[0][1].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "there going to be the chanse for you to convers youre base")
+			bot.Send(msg)
+		}
+		if update.Message.Text == Menu.Keyboard[0][2].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "There we going to use our algorithm")
+			bot.Send(msg)
+		}
+		if update.Message.Text == Menu.Keyboard[1][0].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "if you closed you panel write /start")
+			bot.Send(msg)
+		}
+		if update.Message.Text == Menu.Keyboard[1][1].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Web version in proces")
+			bot.Send(msg)
+		}
+		if update.Message.Text == Menu.Keyboard[1][2].Text {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Yes it is just button")
+			bot.Send(msg)
+		}
+
 	}
 
 }
